1ebbd22d331e4f9bf7651cc1e37e009d13ff65de
sachinchugh <techdomain0502@gmail.com>
2022-01-27 06:35:50 +0530

    lets make a menu for user commands

--- Main.java
+++ Main.java
@@ -8,33 +8,51 @@ public class Main {
 		int id = 1;
 		OrdersService service = new OrdersService();
 		Scanner sc = new Scanner(System.in);
-		String input = "";
+		int input = -1;
 
 		while (true) {
-			input = sc.next();
-			input = input.toLowerCase();
-			switch (input) {
-			case "ad":
-				service.addOrder(new Order(id, Status.INIT));
-				id++;
-				break;
-			case "pg":
-				service.preparingOrder();
-				break;
-			case "pd":
-				service.preparedOrder();
-				break;
-			case "dl":
-				service.delieverOrder();
-				break;
-			case "gcs":
-				service.getOrderCurrentStatus();
-				break;
-			case "shutdown":
-				service.shutDownRestaurant();
-				return;
+			try {
+				System.out.println(
+						     "\n****************************\n"
+						     + "welcome to xyz restaurant\n" + 
+				              "please choose from below . press number \n" 
+								+ "1.add order" // its
+								+ "\n2.prepare order" // for restaurant api
+								+ "\n3.prepared order"// for restaurant api
+								+ "\n4.deliever order"// for restaurant api
+								+ "\n5.get current status of order" // its also endpoint for
+																	// customer to get his order status
+								+ "\n6.shutdown- close restaurant\n"
+								+"\n****************************\n");// for restaurant api
+				input = sc.nextInt();
+				switch (input) {
+				case 1:
+					service.addOrder(new Order(id, Status.INIT));
+					id++;
+					break;
+				case 2:
+					service.preparingOrder();
+					break;
+				case 3:
+					service.preparedOrder();
+					break;
+				case 4:
+					service.delieverOrder();
+					break;
+				case 5:
+					service.getOrderCurrentStatus();
+					break;
+				case 6:
+					service.shutDownRestaurant();
+					return;
+					default:
+						continue;
+				}
+			} catch (Exception e) {
+				System.out.println("invalid entry. please try again");
+				sc.next();
 			}
-		}
 
+		}
 	}
 }
